89a9fd832096662d5727f2a98409b6bde6ac48fb
macha <marie-charlotte.calisto@epita.fr>
2020-12-10 10:10:16 +0100

    CLEAR: remove cpp ConnectionHanler and put all in header. For now it is more practical but may change back

--- include/ConnectionHandler.h
+++ include/ConnectionHandler.h
@@ -9,15 +9,29 @@ class MyConnectionHandler : public AMQP::ConnectionHandler
 	 *  @param  data        memory buffer with the data that should be sent to RabbitMQ
 	 *  @param  size        size of the buffer
 	 */
-	virtual void onData(AMQP::Connection *connection, const char *data, size_t size);
-		
+	virtual void onData(AMQP::Connection *connection, const char *data, size_t size) override
+	{
+		std::cout << "On Data : \"" << data << "\"" << std::endl;
+		// @todo
+		//  Add your own implementation, for example by doing a call to the
+		//  send() system call. But be aware that the send() call may not
+		//  send all data at once, so you also need to take care of buffering
+		//  the bytes that could not immediately be sent, and try to send
+		//  them again when the socket becomes writable again
+	}	
 	/**
 	 *  Method that is called by the AMQP library when the login attempt
 	 *  succeeded. After this method has been called, the connection is ready
 	 *  to use.
 	 *  @param  connection      The connection that can now be used
 	 */
-	virtual void onReady(AMQP::Connection *connection);
+	virtual void onReady(AMQP::Connection *connection) override
+	{
+		std::cout << "Connection established " << std::endl;
+		// @todo
+		//  add your own implementation, for example by creating a channel
+		//  instance, and start publishing or consuming
+	}
 
 	/**
 	 *  Method that is called by the AMQP library when a fatal error occurs
@@ -26,8 +40,14 @@ class MyConnectionHandler : public AMQP::ConnectionHandler
 	 *  @param  connection      The connection on which the error occured
 	 *  @param  message         A human readable error message
 	 */
-	virtual void onError(AMQP::Connection *connection, const char *message);
-
+	virtual void onError(AMQP::Connection *connection, const char *message) override
+	{
+		std::cout << "Connection Error " << message << std::endl;
+		// @todo
+		//  add your own implementation, for example by reporting the error
+		//  to the user of your program, log the error, and destruct the
+		//  connection object because it is no longer in a usable state
+	}
 	/**
 	 *  Method that is called when the connection was closed. This is the
 	 *  counter part of a call to Connection::close() and it confirms that the
@@ -35,7 +55,13 @@ class MyConnectionHandler : public AMQP::ConnectionHandler
 	 *
 	 *  @param  connection      The connection that was closed and that is now unusable
 	 */
-	virtual void onClosed(AMQP::Connection *connection);
+	virtual void onClosed(AMQP::Connection *connection) override
+	{
+		std::cout << "Connection closed" << std::endl;
+		// @todo
+		//  add your own implementation, for example by closing down the
+		//  underlying TCP connection too
+	}
 
 
 };
