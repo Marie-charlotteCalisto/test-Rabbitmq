015b389301035fa1f3ba78117f1b96feff4864cc
macha <marie-charlotte.calisto@epita.fr>
2020-12-16 13:43:18 +0100

    Fix : receive message works, but don't kown exactly why for now

--- src/respond.cpp
+++ src/respond.cpp
@@ -13,32 +13,39 @@ int main()
 	AMQP::TcpConnection connection(&myhandler, adress);
 
 	AMQP::TcpChannel channel(&connection);
+AMQP::Table arguments;
+arguments["x-message-ttl"] = 120 * 1000;
 
-/*	channel.declareExchange("my-exchange", AMQP::direct);
-	channel.declareQueue("my-queue")
-		.onSuccess([]()
-				{
-				std::cout << "queue declared : my-queue" << std::endl;
+// declare the queue
+channel.declareQueue("my-queue", AMQP::durable + AMQP::passive, arguments);
 
-				});
+	// Define callbacks and start
+	auto messageCb = [&channel](
+			const AMQP::Message &message, uint64_t deliveryTag, 
+			bool redelivered)
+	{
+		std::cout << "message received" << std::endl;
+		// acknowledge the message
+		channel.ack(deliveryTag);
+		//processMessage(message.routingkey(), message.body());
+	};
 
+	// callback function that is called when the consume operation starts
+	auto startCb = [](const std::string &consumertag) {
 
-	channel.bindQueue("my-exchange", "my-queue", "my-key")
-		.onSuccess([]()
-				{
-				std::cout << "binded"<< std::endl;
-				});
-	*/			
-	channel.consume("my-exchange");
-	channel.get("my-exchange")
-		.onSuccess([](const AMQP::Message &message, uint64_t deliveryTag, bool redelivered)
-				{
-				std::cout << "Message feteched" << std::endl;
-				})
-	.onEmpty([]()
-			{
-			std::cout << "queue is empty" << std::endl;
-			});
+		std::cout << "consume operation started: " << consumertag << std::endl;
+	};
+
+	// callback function that is called when the consume operation failed
+	auto errorCb = [](const char *message) {
+
+		std::cout << "consume operation failed" << std::endl;
+	};
+
+	channel.consume("my-queue")
+		.onReceived(messageCb)
+		.onSuccess(startCb)
+		.onError(errorCb);
 
 	ev_run(loop, 0);
 
