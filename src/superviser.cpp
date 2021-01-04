9f0ba908917d0d7f4d2d6d93cb06fcdbe4149d37
macha <marie-charlotte.calisto@epita.fr>
2021-01-04 10:43:06 +0100

    ADD : comment for supervisor

--- src/superviser.cpp
+++ src/superviser.cpp
@@ -24,23 +24,28 @@ std::string getNbChannel(CURL *curl)
 
 	std::stringstream httpData;
 
+	//Set url for consumer list
 	curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:15672/api/consumers");
-
 	curl_easy_setopt(curl, CURLOPT_USERPWD, "guest:guest");
+
+	//write data to stringstream (to be parse to json)
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &httpData);
 
 	res = curl_easy_perform(curl);
+
 	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
 
 	if (res == CURLE_OK && httpCode == 200)
 	{
+		//Get json data from string
 		Json::Value jsonData;
 		Json::CharReaderBuilder jsonReader;
 		std::string err;
 
 		if (Json::parseFromStream(jsonReader, httpData, &jsonData, &err))
 		{
+			//return only the number of consumers
 			return std::to_string(jsonData.size());
 		}
 	} 
@@ -53,6 +58,7 @@ int main(void)
 	CURL *curl;
 	std::string readBuffer;
 
+	//TODO: add timer instead of infinit loop
 	while (true)
 	{
 		curl = curl_easy_init();
@@ -63,6 +69,7 @@ int main(void)
 				readBuffer = nbChannel;
 				std::cout << "There is " << readBuffer << " consumer" << std::endl;
 			}
+
 			curl_easy_cleanup(curl);
 		}
 	}
