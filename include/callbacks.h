7a3f03d51a279df10298ae4af96969166f060586
macha <marie-charlotte.calisto@epita.fr>
2021-01-05 09:43:47 +0100

    ADD: comments

--- include/callbacks.h
+++ include/callbacks.h
@@ -5,39 +5,59 @@
 #include <amqpcpp/libev.h>
 #include <unistd.h>
 
-
+/**
+ * \brief callback function that is called when message is received.
+ * add one to the integer received
+ *
+ * TODO: check if received message is an integer
+ * 
+ * @param channel : the channel to acknowledge and publish messages
+ * @param routingkey : the key to be checked
+ * @param publishkey : the key to publish the message with
+ */
 const AMQP::MessageCallback AdditionMessage(
 		AMQP::TcpChannel *channel,
 		std::string routingkey,
 		std::string publishkey);
 
 
-// callback function that is called when the consume operation starts
+/**
+ * \breif callback fuction that is called when the binding of the queue and exchange succeded.
+ * send first message to begin conversation
+ *
+ * @param channel : the channel to publish the message
+ * @param publishkey : the key to publish the message with
+ */
+const AMQP::SuccessCallback bindAndSend(
+		AMQP::TcpChannel *channel,
+		std::string publishkey);
+
+
+/** \brief callback function that is called when the consume operation starts
+ */
 const AMQP::ConsumeCallback startCb = [](const std::string &consumertag) {
 
 	std::cout << "consume operation started: " << consumertag << std::endl;
 };
 
-// callback function that is called when the consume operation failed
+/** \breif callback function that is called when the consume operation failed
+ */
 const AMQP::ErrorCallback errorCb = [](const char *message) {
 
 	std::cout << "consume operation failed : " << message << std::endl;
 };
 
-// callback function that is called when the queue declaration succeded
+/** \brief callback function that is called when the queue declaration succeded
+ */
 const AMQP::SuccessCallback queueDeclared = []() {
 
 	std::cout << "queue declared" << std::endl;
 };
 
-// callback fuction that is called when the binding of the queue and exchange succeded.
+/** \breif callback fuction that is called when the binding of the queue and exchange succeded.
+ */
 const AMQP::SuccessCallback binded = []() {
 
 	std::cout << "binded" << std::endl;
 };
 
-// callback fuction that is called when the binding of the queue and exchange succeded.
-// send first message to begin conversation
-const AMQP::SuccessCallback bindAndSend(
-		AMQP::TcpChannel *channel,
-		std::string publishkey);
