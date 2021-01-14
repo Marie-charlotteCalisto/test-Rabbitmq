26cd2566afd2264c181a6c023a27987ae4b6bb0f
macha <marie-charlotte.calisto@epita.fr>
2021-01-14 12:54:29 +0100

    ADD: callback functions for more clarity

--- include/callbacks.h
+++ include/callbacks.h
@@ -11,14 +11,22 @@
  *
  * TODO: check if received message is an integer
  * 
- * @param channel : the channel to acknowledge and publish messages
- * @param routingkey : the key to be checked
- * @param publishkey : the key to publish the message with
+ * \param client : the client who send the message
+ *
+ * \return lambda messageCallback function that add 1 to the message recieved 
  */
 const AMQP::MessageCallback AdditionMessage(
 		AMQP::TcpChannel *channel,
 		class Client *client);
 
+/**
+ * \brief callback function that is called when queue is declared.
+ * display number of consumers on the queue
+ *
+ * \param connection : the connection to be closed
+ * \param queue : the queue where the consumers exchange
+ */
+const AMQP::QueueCallback displayNbConsumer(AMQP::TcpConnection *connection, std::string queue);
 
 /** \brief callback function that is called when the consume operation starts
  */
@@ -32,4 +40,20 @@ const AMQP::ConsumeCallback startCb = [](const std::string &consumertag) {
 const AMQP::ErrorCallback errorCb = [](const char *message) {
 
 	std::cout << "consume operation failed : " << message << std::endl;
+};
+
+/** \breif callback fuction that is called when the binding of the queue and exchange succeded.
+ */
+const AMQP::SuccessCallback binded = []() {
+
+
+	std::cout << "binded" << std::endl;
+};
+
+/** \brief callback function that is called when the queue declaration succeded
+ */
+const AMQP::SuccessCallback queueDeclared = []() {
+
+
+	std::cout << "queue declared" << std::endl;
 };
