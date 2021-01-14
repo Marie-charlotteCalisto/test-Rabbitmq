59912a9d04439ea1e2d34132e67422bc97b3f472
macha <marie-charlotte.calisto@epita.fr>
2021-01-14 12:58:01 +0100

    CLEAN: code

--- src/callbacks.cpp
+++ src/callbacks.cpp
@@ -5,33 +5,55 @@
 #include "chrono"
 
 const AMQP::MessageCallback AdditionMessage(
-		AMQP::TcpChannel *channel,
-		Client *client)
+        AMQP::TcpChannel *channel,
+        Client *client)
 {
-	return [channel, client](
-		const AMQP::Message &message, uint64_t deliveredTag,
-		bool redelivered)
-	{
-		auto key = message.routingkey();
-		if (key != client->getRoutingkey())
-		{
-			channel->reject(deliveredTag, AMQP::requeue);
-			return;
-		}
+    return [channel, client](
+            const AMQP::Message &message, uint64_t deliveredTag,
+            bool redelivered)
+    {
+        auto key = message.routingkey();
+        if (key != client->getRoutingkey())
+        {
+            channel->reject(deliveredTag, AMQP::requeue);
+            return;
+        }
 
-		// acknowledge the message
-		channel->ack(deliveredTag);
+        // acknowledge the message
+        channel->ack(deliveredTag);
 
-		//get number sent and add one
-		auto messageS =
-			std::stoi(std::string(message.body(), message.body() + message.bodySize())) + 1;
+        //get number sent and add one
+        auto messageS =
+            std::stoi(std::string(message.body(), message.body() + message.bodySize())) + 1;
 
-		std::cout << "message received from " << message.exchange()
-			<< " key " << key << " :\"" << messageS << "\"" << std::endl;
+        std::cout << "message received from " << message.exchange()
+            << " key " << key << ", send back :\"" << messageS << "\"" << std::endl;
 
-		//publish after one second
-		channel->publish(client->getExchange(), client->getPublishkey(), std::to_string(messageS));
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-	};
+        //publish after one second
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        channel->publish(client->getExchange(), client->getPublishkey(), std::to_string(messageS));
+    };
 };
 
+
+const AMQP::QueueCallback displayNbConsumer(AMQP::TcpConnection *connection, std::string queue)
+{
+    return [connection, queue] (
+            const std::string &name, uint32_t messagecount, uint32_t consumercount)
+    {
+        if (consumercount != 2)
+        {
+            std::cout << "consumer crashed on queue : " << queue << " :\""
+                << consumercount<< "\"" << std::endl;
+        }
+        else
+        {
+            std::cout << "Number of consumers : \"" << std::dec << consumercount
+                << "\" from : \"" << queue << "\"" 
+                << " Number of Messages : " << messagecount << std::endl;
+
+        }
+
+        connection->close();
+    };
+}
