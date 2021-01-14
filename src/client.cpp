0ad8385b3374ed555051fa71dd2ee53991a209b2
macha <marie-charlotte.calisto@epita.fr>
2021-01-14 12:58:39 +0100

    ADD: publish function

--- src/client.cpp
+++ src/client.cpp
@@ -3,20 +3,55 @@
 
 void Client::RespondAdditionMessage(Client client)
 {
-	struct ev_loop *loop = ev_loop_new();
-	AMQP::LibEvHandler myhandler(loop);
+    struct ev_loop *loop = ev_loop_new();
+    AMQP::LibEvHandler myhandler(loop);
 
 
-	AMQP::Address adress("amqp://guest:guest@localhost/");
-	AMQP::TcpConnection connection(&myhandler, adress);
-	AMQP::TcpChannel channel(&connection);
+    AMQP::Address adress("amqp://guest:guest@localhost/");
+    AMQP::TcpConnection connection(&myhandler, adress);
+    AMQP::TcpChannel channel(&connection);
 
+    //queue
+    channel.declareQueue(client.getQueue())
+        .onSuccess(queueDeclared);
 
-	channel.consume(client.getQueue())
-		.onReceived(AdditionMessage(&channel, &client))
-		.onSuccess(startCb)
-		.onError(errorCb);
+    //exchange
+    channel.declareExchange(client.getExchange(), AMQP::direct);
+    channel.bindQueue(client.getExchange(), client.getQueue(), client.getRoutingkey())
+        .onSuccess(binded);
 
-	ev_run(loop);
+    //consume
+    channel.consume(client.getQueue())
+        .onReceived(AdditionMessage(&channel, &client))
+        .onSuccess(startCb)
+        .onError(errorCb);
+
+    ev_run(loop);
 };
 
+void Client::publish(std::string message)
+{
+    struct ev_loop *loop = ev_loop_new();
+    AMQP::LibEvHandler myhandler(loop);
+
+    AMQP::Address adress("amqp://guest:guest@localhost/");
+    AMQP::TcpConnection connection(&myhandler, adress);
+    AMQP::TcpChannel channel(&connection);
+
+    //queue
+    channel.declareQueue(queue);
+    //.onSuccess(queueDeclared);
+
+    //exchange
+    channel.declareExchange(exchange, AMQP::direct);
+    channel.bindQueue(exchange, queue, publishkey)
+        .onSuccess([this, &channel, message, loop] ()
+                {
+                channel.publish(exchange, publishkey, message);
+                std::cout << "published : " << message << std::endl;
+                ev_break (loop, EVBREAK_ALL);
+                });
+
+
+    ev_run(loop);
+}
